09011774df923a8624c55bfdeafc169119d9d0b7
Ayyappa Ch <ayyappa.chandolu@amd.com>
2016-10-27 14:54:32 +0530

    drm/amd/amdgpu: Disable Gfx power gating to fix compute hangs
    Due to GFX powergating compute OCL samples are hanging. To fix
    this we disabled gfx powergating.
    
    Signed-off-by: Ayyappa Ch <ayyappa.chandolu@amd.com>

--- drivers/gpu/drm/amd/amdgpu/vi.c
+++ drivers/gpu/drm/amd/amdgpu/vi.c
@@ -1567,12 +1567,13 @@ static int vi_common_early_init(void *handle)
 			AMD_CG_SUPPORT_SDMA_MGCG |
 			AMD_CG_SUPPORT_SDMA_LS;
 	        /* rev0 hardware doesn't support PG */
-	        if (adev->rev_id != 0x00)
+	       /* if (adev->rev_id != 0x00)
 		        adev->pg_flags |= AMDGPU_PG_SUPPORT_GFX_PG |
 				 AMDGPU_PG_SUPPORT_GFX_SMG |
 				 AMDGPU_PG_SUPPORT_GFX_DMG |
 				 AMDGPU_PG_SUPPORT_CP |
 				 AMDGPU_PG_SUPPORT_GFX_PIPELINE;
+	       */
 
 		adev->external_rev_id = adev->rev_id + 0x1;
 		break;
